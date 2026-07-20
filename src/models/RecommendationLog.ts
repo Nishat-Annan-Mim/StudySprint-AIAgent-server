29475fae9e8d1862d7e7231a9b231ebea8137d33
Nishat-Annan-Mim <nishat.annan.mim.2003@gmail.com>
2026-07-20 16:52:45 +0600

    model recomendation  fixaton

--- src/models/RecommendationLog.ts
+++ src/models/RecommendationLog.ts
@@ -1,29 +1,29 @@
-// import mongoose, { Schema, Document, Types } from "mongoose";
+import mongoose, { Schema, Document, Types } from "mongoose";
 
-// export interface IRecommendationLog extends Document {
-//   user: Types.ObjectId;
-//   course: Types.ObjectId;
-//   action: "clicked" | "dismissed" | "enrolled";
-//   timestamp: Date;
-// }
+export interface IRecommendationLog extends Document {
+  user: Types.ObjectId;
+  course: Types.ObjectId;
+  action: "clicked" | "dismissed" | "enrolled";
+  timestamp: Date;
+}
 
-// const RecommendationLogSchema: Schema = new Schema(
-//   {
-//     user: { type: Schema.Types.ObjectId, ref: "User", required: true, index: true },
-//     course: { type: Schema.Types.ObjectId, ref: "Course", required: true },
-//     action: { 
-//       type: String, 
-//       enum: ["clicked", "dismissed", "enrolled"], 
-//       required: true 
-//     },
-//     timestamp: { type: Date, default: Date.now },
-//   },
-//   { 
-//     collection: "recommendationlogs"
-//   }
-// );
+const RecommendationLogSchema: Schema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true, index: true },
+    course: { type: Schema.Types.ObjectId, ref: "Course", required: true },
+    action: { 
+      type: String, 
+      enum: ["clicked", "dismissed", "enrolled"], 
+      required: true 
+    },
+    timestamp: { type: Date, default: Date.now },
+  },
+  { 
+    collection: "recommendationlogs"
+  }
+);
 
-// export const RecommendationLog = mongoose.model<IRecommendationLog>(
-//   "RecommendationLog",
-//   RecommendationLogSchema
-// );
+export const RecommendationLog = mongoose.model<IRecommendationLog>(
+  "RecommendationLog",
+  RecommendationLogSchema
+);
